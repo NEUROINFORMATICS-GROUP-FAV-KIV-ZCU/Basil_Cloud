b56cd57e3e955ef623c4b4d33fcba4af5a097a09
jezekp <jezekp@kiv.zcu.cz>
2019-01-10 09:44:37 +0100

    testing data added

--- scripts/hdfs/dataUpload.sh
+++ scripts/hdfs/dataUpload.sh
@@ -3,11 +3,13 @@ if [[ $# -eq 2 ]] ; then
  dir=$1
  host=$2
  find $dir -type f -name '*.*' -print0 | while IFS= read -r -d '' file; do
- onlyFile="/${dir}"
+  onlyFile=""
  if [[ -d $dir ]]; then
   onlyFile=${file#"$dir"}
+ else 
+   onlyFile=$(basename $dir)
  fi
- LOCATION=$(curl -i --negotiate -u : -X PUT "http://${host}:50070/webhdfs/v1/user/digitalAssistanceSystem${onlyFile}?op=CREATE&overwrite=true" | perl -n -e '/^Location: (.*)$/ && print "$1\n"')
+ LOCATION=$(curl -i --negotiate -u : -X PUT "http://${host}:50070/webhdfs/v1/user/digitalAssistanceSystem/${onlyFile}?op=CREATE&overwrite=true" | perl -n -e '/^Location: (.*)$/ && print "$1\n"')
  URL=${LOCATION%$'\r'}
  curl -i --negotiate -u : -X PUT -T ${file} "${URL}"
  done
